04aa63bea1d0494c66e6c26c28333e60f2536b2d
Ricardo Andres Angel Villadiego <ra.angel10@uniandes.edu.co>
2018-11-18 19:51:09 -0500

    dotenv en server

--- server/main.js
+++ server/main.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import './../imports/api/users';
 import './../imports/api/doodles';
 import './../imports/api/flickr';
+require('dotenv').config();
 
 Meteor.startup(() => {
   // code to run on server at startup
